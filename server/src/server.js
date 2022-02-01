4796f3670c49790c8757d424aaf71a267afe5298
Enes Karakaş <enes.karakas.e@gamil.com>
2022-02-01 17:46:12 +0300

    express setup initiliazed, client files pushed in there

---
+++ server/src/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const app = require('./app');
+
+const PORT = process.env.PORT || 8001;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`listenin port on ${PORT}`);
+});
